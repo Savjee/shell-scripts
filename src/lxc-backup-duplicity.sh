a0fa4eee0c2d8f29127d488b5cbaa6d0d2c9d8c2
Xavier Decuyper <hi@savjee.be>
2017-04-04 14:32:55 +0200

    New LXC backup script with duplicity

---
+++ src/lxc-backup-duplicity.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# This script goes into the LXC storage directory and takes a backup of
+# each subfolder with duplicity. It then uploads the backups (incrementally) to Backblaze B2
+#
+# Author: Xavier Decuyper <www.savjee.be>
+# ---------------------------------------------------------------------------
+
+cd /lxc-host/
+
+# Used to encrypt backups
+export PASSPHRASE="xxxxxxxxx"
+
+B2_BUCKET="xxxxxxxxx"
+B2_ACCOUNT="xxxxxxxxx"
+B2_KEY="xxxxxxxxx"
+
+if ! [ $(which duplicity 2>/dev/null) ]; then
+   echo "Duplicity is not installed. Can't continue."
+   exit 1;
+fi
+
+for dir in */
+do
+  dir=$(basename "$dir")
+
+  echo "Starting backup for $dir ..."
+  duplicity --exclude-filelist /root/exclude-backup.txt $dir b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET}/$dir/ --full-if-older-than 30D --numeric-owner
+
+  echo "Deleting old backups on B2..."
+  duplicity remove-all-but-n-full 2 --force b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET}/$dir/
+done
+
+unset PASSPHRASE
